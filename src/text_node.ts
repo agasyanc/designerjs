b386d94b438b37893a64e6590fa668c92b9216e4
Evgeniy <agasyanc@gmail.com>
2024-06-12 09:42:47 +0400

    simple multiline text test

--- src/text_node.ts
+++ src/text_node.ts
@@ -5,27 +5,44 @@ import SceneNode from "./scene_node";
 export default class TextNode extends SceneNode{
   font:HarfBuzzFont|null = null;
   size:number = 16;
-  text:string = 'Hello';
+  text:string = 'Hello\ncmds\n,vkdfsml\n\n,vlds' + String.fromCharCode(Math.random() * 100);
   position:Point = new Point(0, 0);
+  interliniage:number = 1.2;
+  cached:Path2D|null = null;
   
   constructor(){
       super();
   }
+
   render(ctx: CanvasRenderingContext2D): void {
     
     if (this.font) {
-      let x = this.position.x;
-      const scale = this.size / this.font.unitsPerEM
-      const shapes = this.font.shape(this.text);
-      console.log(shapes);
-      
-      const path2d_text = new Path2D();
-      for (const shape of shapes) {
-        path2d_text.addPath(new Path2D(this.font.glyphToPath(shape.GlyphId)), new DOMMatrix().translate(x, this.position.y).flipY().scale(scale));
-        x += shape.XAdvance * scale;
+      if (!this.cached) {
+        const lines = this.text.split('\n');
+
+        let x = 0;
+        let y = 0;
+        const path2d_text = new Path2D();
+        const scale = this.size / this.font.unitsPerEM
+        for (const line of lines) {
+          let shapes = this.font.shape(line);
+          for (const shape of shapes) {
+            path2d_text.addPath(new Path2D(this.font.glyphToPath(shape.GlyphId)), new DOMMatrix().translate(x, y).flipY().scale(scale));
+            x += shape.XAdvance * scale;
+          }
+          x = 0
+          y += this.size * this.interliniage;
+        }
+       
+        this.cached = path2d_text;
       }
-      ctx.fill(path2d_text);
+      ctx.save()
+      ctx.translate(this.position.x, this.position.y)
+      ctx.fill(this.cached);
+      ctx.restore()
       return;
+      
+      
     }
     ctx.save()
     ctx.font = this.size + 'px system-ui';
